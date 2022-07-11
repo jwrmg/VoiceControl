02cc75a8c74c5be3af13795125bc5f7a8b14c4e6
James Weston <unknownkoba@gmail.com>
2022-07-11 19:28:37 +1000

    Commit 3?

--- VoiceControlApplication/Program.cs
+++ VoiceControlApplication/Program.cs
@@ -13,32 +13,6 @@ namespace VoiceControlApplication
             {
                 ApplicationDriver.Limit(16);
             }
-
-            CommandReceipt receipt = new();
-            CommandBlueprint entry = new CommandBlueprint()
-            { };
-
-
-            receipt = CommandReceipt.Load("commands.txt");
-
-            //receipt.commands.Add("test", new CommandBlueprint()
-            //{
-            //    textToRecognize = "EmptyText",
-            //    commandArgs = new[] { "emptyArgs" },
-            //    commandFunction = "TestFunction",
-            //    commandDelegateNamespace = "VoiceControl.Command.CommandHelper"
-            //});
-            //
-            ////public string textToRecognize = "EmptyText";
-            ////public string[] commandArgs = { "emptyArguments" };
-            ////public string commandFunction = "EmptyFunction";
-            ////public string commandDelegateNamespace = "VoiceControl.Command.CommandHelper";
-            //
-            //CommandReceipt.Save("commands.txt", receipt);
-
-            CommandDatabase database = new(receipt);
-
-            database.commands["test"].command?.Invoke(new[] { "Hello", "There,", "General Kenobi" });
         }
     }
 }
